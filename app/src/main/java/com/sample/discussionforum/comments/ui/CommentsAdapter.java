44fa801cb8ff14380a93f136b0beab65c3459020
Ankit <ankit.goyal@vymo.in>
2019-07-07 18:29:11 +0530

    migrated to Android jetpack lib.

--- app/src/main/java/com/sample/discussionforum/comments/ui/CommentsAdapter.java
+++ app/src/main/java/com/sample/discussionforum/comments/ui/CommentsAdapter.java
@@ -1,12 +1,12 @@
 package com.sample.discussionforum.comments.ui;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
